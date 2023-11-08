21b34f598e91a46107f6118596f22dd5d78fa91e
DABO26 <diego.breton@alumnos.ucn.cl>
2023-11-08 10:58:22 -0300

    Avance (por terminar)

--- app/src/main/java/com/example/prctica2/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/prctica2/ui/home/HomeFragment.java
@@ -1,20 +1,36 @@
 package com.example.prctica2.ui.home;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.prctica2.R;
 import com.example.prctica2.databinding.FragmentHomeBinding;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+
+    private ListView lista_Consola;
+
+    private EditText txt_Nombre, txt_Descripcion;
+
+    private ArrayList<String> listaNombres = new ArrayList<>();
+    private ArrayList<String> listaDescripciones = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,9 +40,43 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+
+        txt_Nombre = binding.txtNombre;
+        txt_Descripcion = binding.txtDescripcion;
+
+        binding.btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                agregar();
+            }
+        });
+
         return root;
+    }
+
+    public void agregar() {
+        lista_Consola = binding.listaConsolas;
+
+
+
+
+        listaNombres.add(txt_Nombre.getText().toString());
+        listaDescripciones.add(txt_Descripcion.getText().toString());
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this.getContext(), R.layout.item_list, listaNombres);
+
+        lista_Consola.setAdapter(adapter);
+
+        final Context context = this.getContext();
+        lista_Consola.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+                String mensaje = "Nombre: " + lista_Consola.getItemAtPosition(i) + ". Descripción: "
+                        + listaDescripciones.get(i);
+                Toast.makeText(context, mensaje, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
